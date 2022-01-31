0a02a7bd79d3bcc76f995719bbf78b9082ec7b13
Juan González <juangonzalezmontes@gmail.com>
2022-01-31 20:19:07 +0100

    course/section-8
    - Add Web Profiler
    - Add Logger

--- src/Controller/HealthCheckController.php
+++ src/Controller/HealthCheckController.php
@@ -4,15 +4,22 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HealthCheckController
 {
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
     #[Route('/', name: 'health_check', methods: ['GET'])]
     public function __invoke(): Response
     {
+        $this->logger->error('This is the HealthCheckController error');
+
         return new JsonResponse(['status' => 'ok']);
     }
 }
