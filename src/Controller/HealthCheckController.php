dbb84d93357b503bd44df4f7af1484b4fb9da03c
Juan González <juangonzalezmontes@gmail.com>
2022-02-15 12:21:44 +0100

    course/section-17
    - Add template for homepage
    - Add menu items

--- src/Controller/HealthCheckController.php
+++ src/Controller/HealthCheckController.php
@@ -15,7 +15,7 @@ class HealthCheckController
     {
     }
 
-    #[Route('/', name: 'health_check', methods: ['GET'])]
+    #[Route('/health-check', name: 'health_check', methods: ['GET'])]
     public function __invoke(): Response
     {
         $this->logger->error('This is the HealthCheckController error');
